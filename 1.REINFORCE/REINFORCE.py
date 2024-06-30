ce05285d415cad9b53b27d7f8eb753135017545b
Mr-xiaotian <75198199+Mr-xiaotian@users.noreply.github.com>
2024-06-30 22:32:13 +0800

    [Refine] some bug in refinforce

--- 1.REINFORCE/REINFORCE.py
+++ 1.REINFORCE/REINFORCE.py
@@ -71,12 +71,13 @@ def evaluate_policy(env, agent):
     times = 3  # Perform three evaluations and calculate the average
     evaluate_reward = 0
     for _ in range(times):
-        s = env.reset()
+        s, _ = env.reset()
         done = False
         episode_reward = 0
         while not done:
             a = agent.choose_action(s, deterministic=True)  # We use the deterministic policy during the evaluating
-            s_, r, done, _ = env.step(a)
+            s_, r, done, truncated, _ = env.step(a)
+            done = done or truncated
             episode_reward += r
             s = s_
         evaluate_reward += episode_reward
@@ -86,54 +87,58 @@ def evaluate_policy(env, agent):
 
 if __name__ == '__main__':
     env_name = ['CartPole-v0', 'CartPole-v1']
-    env_index = 0  # The index of the environments above
+    env_index = 1  # 环境索引
     env = gym.make(env_name[env_index])
-    env_evaluate = gym.make(env_name[env_index])  # When evaluating the policy, we need to rebuild an environment
+    env_evaluate = gym.make(env_name[env_index])  # 评估时需要重新构建环境
     number = 1
-    seed = 0
-    env.seed(seed)
-    env_evaluate.seed(seed)
+    seed = 500
+
+    # 设置随机种子
+    env.reset(seed=seed)
+    env_evaluate.reset(seed=seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.n
-    max_episode_steps = env._max_episode_steps  # Maximum number of steps per episode
+    max_episode_steps = env._max_episode_steps  # 每个episode的最大步数
     print("state_dim={}".format(state_dim))
     print("action_dim={}".format(action_dim))
     print("max_episode_steps={}".format(max_episode_steps))
 
     agent = REINFORCE(state_dim, action_dim)
-    writer = SummaryWriter(log_dir='runs/REINFORCE/REINFORCE_env_{}_number_{}_seed_{}'.format(env_name[env_index], number, seed))  # build a tensorboard
+    writer = SummaryWriter(log_dir='1.REINFORCE/runs/REINFORCE/REINFORCE_env_{}_number_{}_seed_{}'.format(env_name[env_index], number, seed))  # 构建tensorboard
 
-    max_train_steps = 1e5  # Maximum number of training steps
-    evaluate_freq = 1e3  # Evaluate the policy every 'evaluate_freq' steps
-    evaluate_num = 0  # Record the number of evaluations
-    evaluate_rewards = []  # Record the rewards during the evaluating
-    total_steps = 0  # Record the total steps during the training
+    max_train_steps = 1e5  # 最大训练步数
+    evaluate_freq = 1e3  # 每隔evaluate_freq步评估一次策略
+    evaluate_num = 0  # 记录评估次数
+    evaluate_rewards = []  # 记录评估奖励
+    total_steps = 0  # 记录训练的总步数
 
     while total_steps < max_train_steps:
         episode_steps = 0
-        s = env.reset()
+        s, _ = env.reset()
         done = False
         while not done:
             episode_steps += 1
             a = agent.choose_action(s, deterministic=False)
-            s_, r, done, _ = env.step(a)
+            s_, r, done, truncated, _ = env.step(a)
+            done = done or truncated
             agent.store(s, a, r)
             s = s_
 
-            # Evaluate the policy every 'evaluate_freq' steps
+            # 每隔evaluate_freq步评估一次策略
             if (total_steps + 1) % evaluate_freq == 0:
                 evaluate_num += 1
                 evaluate_reward = evaluate_policy(env_evaluate, agent)
                 evaluate_rewards.append(evaluate_reward)
-                print("evaluate_num:{} \t evaluate_reward:{} \t".format(evaluate_num, evaluate_reward))
-                writer.add_scalar('step_rewards_{}'.format(env_name[env_index]), evaluate_reward, global_step=total_steps)
+                print(f"evaluate_num:{evaluate_num} \t evaluate_reward:{evaluate_reward} \t")
+                writer.add_scalar(f'step_rewards_{env_name[env_index]}', evaluate_reward, global_step=total_steps)
                 if evaluate_num % 10 == 0:
-                    np.save('./data_train/REINFORCE_env_{}_number_{}_seed_{}.npy'.format(env_name[env_index], number, seed), np.array(evaluate_rewards))
+                    np.save(f'1.REINFORCE/data_train/REINFORCE_env_{env_name[env_index]}_number_{number}_seed_{seed}.npy', 
+                            np.array(evaluate_rewards))
 
             total_steps += 1
 
-        # An episode is over,then update
+        # 一个episode结束后进行更新
         agent.learn()
